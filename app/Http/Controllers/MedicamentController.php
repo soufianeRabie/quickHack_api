bc021c82048817de006e95a2550de985d2808c1f
soufianeRabie <rabyasoufiane@gmail.com>
2024-06-08 09:44:19 +0100

    create medicament table

---
+++ app/Http/Controllers/MedicamentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Medicament;
+use Illuminate\Http\Request;
+
+class MedicamentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Medicament $medicament)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Medicament $medicament)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Medicament $medicament)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Medicament $medicament)
+    {
+        //
+    }
+}
